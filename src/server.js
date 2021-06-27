83f8edbe67384c459e37c7f7df8ec2255552976b
Arthur Siqueira <arthur.siq5@gmail.com>
2021-06-26 22:29:49 -0300

    Puxados dados da view no backend

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ server.set('view engine', 'ejs')
 server.use(express.static('public'))
 server.set('views', path.join(__dirname, 'view'))
 
+server.use(express.urlencoded({extended: true}))
+
 server.use(route)
 
 server.listen(3000, () => console.log('RODANDO'))
