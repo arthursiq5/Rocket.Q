955982e506dfb4ba929fe07ee07a9980b21f3d4c
Arthur Siqueira <arthur.siq5@gmail.com>
2021-06-27 18:00:09 -0300

    Garantindo que o ID da sala seja único

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,11 +1,36 @@
-module.exports = {
-    create(req, res){
-        let roomId = ''
+const Database = require('../db/config')
 
-        for (let index = 0; index < 6; index++) {
-            roomId += Math.floor(Math.random() * 10).toString()
-            
+module.exports = {
+    async create(req, res){
+        const db = await Database()
+        const pass = req.body.password
+        let roomId = ''
+        let isRoom = true
+
+        while(isRoom) {
+            for (let index = 0; index < 6; index++) {
+                roomId += Math.floor(Math.random() * 10).toString()
+                
+            }
+    
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+    
+            isRoom = roomsExistIds.some(roomExistId => roomId === roomExistId)
+    
+            if (!isRoom) {
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }else{
+                roomId = ''
+            }
         }
+
+        await db.close()
 
         res.redirect(`/room/${roomId}`)
     }
